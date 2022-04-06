ccedc74057df99cefa91501d634a768a6f794298
Alex_Koss <cahek27@mail.ru>
2022-04-06 07:25:23 +0300

    HW6

---
+++ resources/views/admin/news/index.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.admin')
+@section('content')
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h2">Список новостей</h1>
+    <div class="btn-toolbar mb-2 mb-md-0">
+        <div class="btn-group me-2">
+            <a href="{{ route('admin.news.create') }}" class="btn btn-sm btn-outline-secondary">Добавить новость</a>
+        </div>
+    </div>
+</div>
+
+<div class="table-responsive">
+    @include('inc.messages')
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>#ID</th>
+                <th>Категория</th>
+                <th>Заголовок</th>
+                <th>Статус</th>
+                <th>Описание</th>
+                <th>Дата редактирования</th>
+                <th>Опции</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse($newsList as $news)
+            <tr>
+                <td>{{ $news->id }}</td>
+                <td>{{ $news->category->title }}</td>
+                <td>{{ $news->title }}</td>
+                <td>{{ $news->status }}</td>
+                <td>{{ $news->description }}</td>
+                <td>@if($news->updated_at) {{ $news->updated_at->format('d-m-Y H:i') }} @endif </td>
+                <td>
+                    <a href="{{ route('admin.news.edit', ['news' => $news]) }}">Ред.</a>
+                    &nbsp;
+                    <a href="javascript:;" style="color:red;">Удл.</a>
+                </td>
+            </tr>
+            @empty
+            <tr>
+                <td colspan="4">Записей нет</td>
+            </tr>
+            @endforelse
+        </tbody>
+    </table>
+
+    {{ $newsList->links() }}
+</div>
+@endsection
